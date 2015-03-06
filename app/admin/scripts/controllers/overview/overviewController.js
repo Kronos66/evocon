781f6920e575664d5e58e3cfac646cdb95654c1b
Szymon M <szymon.maslowski@vp.pl>
2015-03-06 12:30:52 +0100

    overview ready

--- app/admin/scripts/controllers/overview/overviewController.js
+++ app/admin/scripts/controllers/overview/overviewController.js
@@ -9,20 +9,52 @@
                     .then( function( data ) {
                             ctrl.stations = data;
                             getDetails();
-                            console.log( data );
                         } );
             },
             getDetails = function() {
-                angular.forEach(ctrl.stations, function( element ) {
+                angular.forEach( ctrl.stations, function( element ) {
                     StationsDAO.get( element.stationId )
                         .then( function( data ) {
-                                console.log( data );
-                            element.details = data;
+                            angular.forEach( ctrl.data, function( val, prop ) {
+                                val.push( data.hasOwnProperty( prop ) ? trueFalseOrValue( data[ prop ] ) : '' );
+                            } );
                         } );
                 } );
-                console.log( ctrl.stations );
+
+            },
+            trueFalseOrValue = function( val )
+            {
+                if( val === true ) {
+                    return 'YES';
+                }
+                else if( val === false ) {
+                    return 'NO';
+                }
+
+                return val;
             };
 
+
+        ctrl.data = {
+            name: [],
+            description: [],
+            speedSlowLimit: [],
+            lineNumber: [],
+            groupId: [],
+            markerQty: [],
+            colorMode: [],
+            speedNormalLimit: [],
+            speedTooSlowLimit: [],
+            unit: [],
+            autoStanby: [],
+            effMarkersEnabled: [],
+            offFilter: [],
+            signalInversion: [],
+            stopTime: [],
+            enabled: [],
+            standbyEnabled: [],
+            onFilter: []
+        };
 
 
         refresh();
