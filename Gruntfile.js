8c287d2702007ea60ffb121b4981cb7bdb04f05e
maksymilian.pamula <maksymilian.pamula@gmail.com>
2015-03-09 14:30:21 +0100

    deploy  on heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,7 @@ module.exports = function (grunt)
         // The actual grunt server settings
         connect: {
             options: {
-                port: 9000, // Change this to '0.0.0.0' to access the server from outside.
+                port: process.env.PORT || 9000, // Change this to '0.0.0.0' to access the server from outside.
                 hostname: '*',
                 livereload: 35729
             },
